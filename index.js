201d083ad54655fee0f0b6cc353569cbf9c9ccf9
+Bridgette <+sofiaeuc112002@gmail.com+>
2023-09-29 10:45:26 -0500

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = parseInt(Math.random() * 100)
+
+let num3 = 11
+let num4 = 7
+
+let mod = num3 % num4
+
+let max = Math.max(20, 10)
+
